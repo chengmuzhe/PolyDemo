86c16461b5a585ec01c29e5d4556c8f9285e80cc
cmz <1356473856@qq.com>
2018-07-24 17:47:03 +0800

    第一次推送

---
+++ PolyMemo/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PolyMemo
+{
+    class Person
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public void Display()
+        {
+            Console.WriteLine("Person Display");
+        }
+        public virtual void Display1()
+        {
+            Console.WriteLine("Person1 Display");
+        }
+
+     public    class Student:Person
+        {
+            public new void Display()
+            {
+                Console.WriteLine("Student Display");
+            }
+            public override void Display1()
+            {
+                Console.WriteLine("Student1 Display");
+            }
+        }
+
+    }
+}
---
+++ PolyMemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace PolyMemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new PolyMemo.Person.Student();
+
+            p.Display();
+            p.Display1();
+            Console.ReadLine();
+        }
+    }
+}
